a40b98733e8ac7a87a1d0376f5c63a68b3b6f4ac
taro.kirameki <fooleeqaa@gmail.com>
2020-02-16 11:11:48 +0000

    End

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,18 +3,18 @@ class TasksController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show, :new, :edit]
   
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks.order(id: :desc).page(params[:page])
   end
   
   def show 
   end
   
   def new 
-    @task = Task.new 
+    @task = current_user.tasks.build
   end
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
      
      if @task.save
        flash[:success] = "新しいタスクを作成しました"
--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_15_123446) do
+ActiveRecord::Schema.define(version: 2020_02_16_102849) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
-  end
-
-  create_table "taskusers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "content"
     t.bigint "user_id"
-    t.string "status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_taskusers_on_user_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -36,5 +29,5 @@ ActiveRecord::Schema.define(version: 2020_02_15_123446) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "taskusers", "users"
+  add_foreign_key "tasks", "users"
 end
